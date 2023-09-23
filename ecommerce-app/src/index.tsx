bb9d112e0c6bfeb1e79e62a7eba84cd381a7362a
Rudransh Arora <rudransh1482@gmail.com>
2023-09-23 21:00:08 +0530

    Made ecommerce website

---
+++ ecommerce-app/src/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { GoogleAuthProvider } from "firebase/auth";
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "MEASUREMENT_ID"
+};
+
+
+export const provider = new GoogleAuthProvider();
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+ReactDOM.render(
+   <React.StrictMode>
+      <BrowserRouter>
+         <Provider store={store}>
+            <App />
+         </Provider>
+      </BrowserRouter>
+   </React.StrictMode>,
+   document.getElementById("root")
+);
